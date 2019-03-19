32abecd66feb3061f51bbacc8887bbb17a00c052
Tatiana Pascal <josephinex21@gmail.com>
2019-03-19 11:40:03 +0200

    Delete v1_2__fill_emp_table.sql

--- src/main/resources/db/migration/v1_2__fill_emp_table.sql
+++
@@ -1,6 +0,0 @@
-insert into employee(id, emp_name, age) values (5, 'Jeff', 54);
-insert into employee(id, emp_name, age) values (6, 'Tania', 31);
-insert into employee(id, emp_name, age) values (7, 'Alexei', 23);
-insert into employee(id, emp_name, age) values (8, 'Jeff', 54);
-insert into employee(id, emp_name, age) values (9, 'Alice', 31);
-insert into employee(id, emp_name, age) values (10, 'Bob', 23);
